4990bd6068fce9a93a977c6fac53d45d1ca83e8b
Brian.dev <62214238+Brian3720@users.noreply.github.com>
2023-10-14 17:56:49 -0300

    Add files via upload

--- 034.py
+++ 034.py
@@ -3,4 +3,8 @@ Escreva um programa que leia 10 números, se for ímpar deve ser descartado, se 
 '''
 
 for i in range(1,11):
+    Numero = int(input('Digite seu número'))
+    if Numero % 2 == 0:
+        soma += Numero
 
+print(f'A soma dos pares é {soma}')
--- 035.py
+++ 035.py
@@ -7,14 +7,30 @@ Qual é o homem mais velho
 Quantas mulheres têm menos de 20 anos
 '''
 
-nome = []
-sexo = []
-idade = []
+Soma_idade = 0
+Idade_Homem_Mais_Velho = 0
+Nome_Homem_Mais_Velho = ''
+Quant_Mulheres_Menor_20_anos = 0
 
-for i in range(1, 5, +1):
-    print("cadastro de cliente")
-    nome.append(input("digite um nome: "))
-    sexo.append(input("Masculino / Feminino?: ").lower())
+for i in range(1, 5):
+
+    print('-----------------------------------')
+    nome = input('Digite um nome: ')
+    idade = int(input('Digite a idade: '))
+    sexo = input('Digite o sexo [M], [F]: ')
 
 
-print(nome)
+    Soma_idade += idade
+
+    if sexo == 'M' and Idade_Homem_Mais_Velho < idade:
+        Idade_Homem_Mais_Velho = idade
+        Nome_Homem_Mais_Velho = nome
+
+    if sexo == 'F' and idade < 20:
+        Quant_Mulheres_Menor_20_anos += 1
+
+Media_idade = Soma_idade / 4
+
+print(f'A média de idade dos grupos é {Media_idade}'
+      f'\nO homem mais velho é {Nome_Homem_Mais_Velho} e a idade dele é {Idade_Homem_Mais_Velho}'
+      f'\nExistem {Quant_Mulheres_Menor_20_anos} menores de 20 anos')
