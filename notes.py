2d95c056cd1e41812320300208f3d29b283aa7db
Brian.dev <62214238+Brian3720@users.noreply.github.com>
2023-10-28 17:56:39 -0300

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,15 @@
+'''
+contador = 0
+
+while contador < 5:
+print(f'O contador é {contador}')
+contador = contador + 1
+'''
+
+#----------------------------------------------------
+
+'''
+escolha = ''
+while escolha != 'N':
+escolha = input('Deseja continuar? [S/N]')
+'''
